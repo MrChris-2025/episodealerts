acb3b4a9538d0bd3b2876afc74e37210a35e474c
MrChris-2025 <7rings@protonmail.com>
2026-08-13 07:09:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
 self.addEventListener('push', function(event) {
-  let data = { title: 'Alert', body: 'New content is available!' };
-  if (event.data) { data = event.data.json(); }
+  let data = { title: 'New Alert', body: 'New content is available!' };
+  
+  if (event.data) {
+    data = event.data.json();
+  }
+
   const options = {
     body: data.body,
-    icon: './icon-512.png',
-    badge: './icon-512.png',
-    vibrate: [100, 50, 100]
+    icon: 'https://via.placeholder.com/128/09f/fff.png',
+    badge: 'https://via.placeholder.com/128/09f/fff.png',
+    vibrate: [200, 100, 200]
   };
-  event.waitUntil(self.registration.showNotification(data.title, options));
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 });
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-  event.waitUntil(clients.openWindow('/'));
+  event.waitUntil(
+    clients.openWindow('/')
+  );
 });
