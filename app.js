cfc4cac3cfa1460f119040a815e8e2bc19b43dba
MrChris-2025 <7rings@protonmail.com>
2026-08-18 18:27:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Initialize Back4App Parse SDK
-Parse.initialize("YOUR_BACK4APP_APP_ID", "YOUR_BACK4APP_JS_KEY");
+Parse.initialize("gH0Ry12pUmKdlIWwjbDtN5T8lCkoZnfD6Xp9rvoq", "bSh7EVVqy3oQMUup6qDZQBVax28RmVeGgE92tMlp");
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 const TMDB_API_KEY = '1070730380f5fee0d87cf0382670b255';
-const VAPID_PUBLIC_KEY = 'YOUR_VAPID_PUBLIC_KEY'; // Must match backend VAPID public key
+const VAPID_PUBLIC_KEY = 'BC-LY0azo2sZzvZ4ZoQnZwnpLpIwhrOFsDTQ9YbiuSdWLNqKQYdNGmMM9Am6IH-Zd9rBPg7gcXOEYiFyNsz2Fh8'; // Must match backend VAPID public key
 
 let currentType = 'tv'; // 'tv' (TVmaze) or 'movie' (TMDB)
 let allItems = [];
